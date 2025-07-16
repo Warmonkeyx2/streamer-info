3cc5805d4c36012206d759ab6c9a7aa4988deb1b
WARMONKEYX <132633130+Warmonkeyx2@users.noreply.github.com>
2025-07-16 17:06:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function showTab(tabId) {
   tabs.forEach(tab => tab.style.display = 'none');
   document.getElementById(tabId).style.display = 'block';
 }
+
+
+let currentColor = '#00f0ff'; // Default color
+
+function toggleColorPicker() {
+  const picker = document.getElementById("colorPicker");
+  picker.style.display = picker.style.display === "block" ? "none" : "block";
+}
+
+function updateThemeColor(hexColor) {
+  currentColor = hexColor;
+
+  document.documentElement.style.setProperty('--accent-color', currentColor);
+}
