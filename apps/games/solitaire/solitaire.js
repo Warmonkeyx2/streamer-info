929ff78e7da72cea9bf59bf361b1d0ad07eefad5
WARMONKEYX <132633130+Warmonkeyx2@users.noreply.github.com>
2025-07-18 16:09:39 -0400

    Update solitaire.js

--- apps/games/solitaire/solitaire.js
+++ apps/games/solitaire/solitaire.js
@@ -1,1 +1,7 @@
 
+function restartSolitaire() {
+  alert("Game would restart here. This is a placeholder.");
+}
+
+// Automatically called when the HTML is injected into the draggable popup
+console.log("🃏 Solitaire game script loaded.");
